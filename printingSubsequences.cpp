fddec388e89f27e78fe48d957af6ab4e798c0ac3
Procoder16 <smsoumik1608@gmail.com>
2022-05-10 11:07:33 +0530

    printing subsequences code added

---
+++ printingSubsequences.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void subseq(int arr[], int i, int n, vector<int> &ans){
+    if(i == n){
+        for(auto it : ans){
+                cout<<it<<" ";
+        }
+        if(ans.size() == 0){
+            cout<<"{}";
+        }
+        cout<<endl;
+        return;
+    }
+    ans.push_back(arr[i]);
+    subseq(arr, i + 1, n, ans);
+    ans.pop_back();
+    subseq(arr, i + 1, n, ans);
+}
+ 
+int main(){
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+
+    int arr[] = {3, 1, 2};
+
+    vector<int> ans = {};
+    subseq(arr, 0, 3, ans);
+
+    return 0;
+}
